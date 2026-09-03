e798f2330934e2b5fdfd61d2753f2b93bf6fe03a
ambitionsnacks1-developer <nabulovicky1@gmail.com>
2026-09-03 16:08:33 +0300

    Add files via upload

--- CarSystem.java
+++ CarSystem.java
@@ -1,35 +1,32 @@
- import java.util.scanner;
- public class CarSystem{
-    public static void main(String[] args){
+import java.util.Scanner;
 
-        String  Available Car, Functionality;
-        double  Price;
-        int choice, Quantity;
-        scanner input = new Scanner(System.in);
+public class CarSystem {
+    public static void main(String[] args) {
 
-        
+        String AvailableCar, Functionality;
+        double Price;
+        int Choice, Quantity;
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Enter the Available Cars:");
-        Available Car = input.nextString();
+        AvailableCar = input.next();
         System.out.println("Enter Price:");
-        Price = input.nextdouble();
+        Price = input.nextDouble();
         System.out.println("Enter the Choice:");
-        Choice = input.nextint();
+        Choice = input.nextInt();
         System.out.println("Enter the Quantity:");
-        Quantity = input.nextint();
+        Quantity = input.nextInt();
         System.out.println("Enter Functionality:");
-        Funtionality = input.nextstring();
+        Functionality = input.next();
 
-        System.out.println("\---Car details---");
+        // System.out.println("---Car details/-");
         System.out.println("Car: " + AvailableCar);
         System.out.println("Price: " + Price);
-        System.out.println("choice: " + Choice)
+        System.out.println("choice: " + Choice);
         System.out.println("Quantity: " + Quantity);
-        system.outt.println("Functionality:" + Functionality);
-
+        System.out.println("Functionality:" + Functionality);
 
         input.close();
 
-        
-        
     }
- }
+}
